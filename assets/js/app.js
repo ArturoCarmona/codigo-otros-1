28606d3e830424344f5c053c2193017f36e5d551
Arturo Guzman <gab_r2ro@hotmail.com>
2022-10-29 16:31:24 -0500

    fix: se arreglo funcion displayUsers, ahora ya funciona

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,38 @@
+/**
+ * El programa hace una solicitud fetch para obtener y mostrar 
+ * usuarios y desplegarlos en pantalla.
+ */
+
+
+//Quitar el signo $ al inicio de cada variable, no esta permitido
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//SE cambio .name, .blog (class)
+//Se cambio tipo const por let
+let n = document.querySelector('.name');
+let b = document.querySelector('.blog');
+let l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//url ='https://api.github.com/users/stolinski'
+//Se quito await al principio de la funcion
+//Se agrego .catch en caso de error
+//Se agrego .innerHTML para mostrar resultado en pantalla
+
+function displayUser(username){
+  n.textContent = 'cargando...'
+  fetch(`${usersEndpoint}/${username}`)
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+    n.innerHTML = response.name;
+    b.innerHTML = response.blog;
+    l.innerHTML = response.location;
+  })
+  .catch(error => {
+    console.log("OH NO!");
+    console.log(error);
+    n.innerHTML = `Algo salió mal: ${err}`
+  });
 }
 
 function handleError(err) {
@@ -18,5 +40,5 @@ function handleError(err) {
   console.log(err);
   n.textContent = `Algo salió mal: ${err}`
 }
-
-displayUser('stolinski').catch(handleError);
+//SE quito .catch y se agrego en la solicitud fetch
+displayUser('stolinski');
